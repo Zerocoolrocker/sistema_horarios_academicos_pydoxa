2c22e7f2e7f7084cbe03b2905afb95750c971a3b
Ricardo Bello <zerocoolrocker@gmail.com>
2019-11-03 11:56:22 -0430

    nuevos modelos base

--- core/models.py
+++ core/models.py
@@ -6,10 +6,18 @@ class Area(models.Model):
 	nombre = models.CharField(max_length=100)
 	created = models.DateField(auto_now_add=True)
 	updated = models.DateField(auto_now=True)
-	usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+	# usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.nombre	
+
+class Aula(models.Model):
+	nombre = models.CharField(max_length=60, blank=True, null=True)
+	# tipo_aula = models.ForeignKey('TipoAula', on_delete=models.CASCADE)
+	ubicacion = models.ForeignKey('UbicacionAula', on_delete=models.CASCADE)
+	# proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)		
 
 class Carrera(models.Model):
 	nombre = models.CharField(max_length=60)
@@ -22,7 +30,7 @@ class Carrera(models.Model):
 class Pensum(models.Model):
 	nombre = models.CharField(max_length=60)
 	fecha = models.DateField()
-	regimen = models.CharField(max_length=10)
+	# regimen = models.CharField(max_length=10)
 	carrera = models.ForeignKey('Carrera', on_delete=models.CASCADE)
 
 	def __str__(self):
@@ -33,7 +41,7 @@ class Proyecto(models.Model):
 	pensum = models.ForeignKey('Pensum', on_delete=models.CASCADE)
 	lapso_academico = models.CharField(max_length=10)
 	fecha = models.DateField()
-	fecha_memo = models.DateField()
+	fecha_memo = models.DateField(blank=True, null=True)
 	observaciones = models.TextField(blank=True, null=True)
 	created = models.DateTimeField(auto_now_add=True)
 	update = models.DateTimeField(auto_now=True)
@@ -41,33 +49,119 @@ class Proyecto(models.Model):
 	def __str__(self):
 		return "%s - %s" % (self.nombre, self.lapso_academico)
 
-class Direccion(models.Model):
-	nombre = models.CharField(max_length=30)
-	area = models.ForeignKey('Area', on_delete=models.CASCADE)
-
-class Departamento(models.Model):
-	nombre = models.CharField(max_length=30)
-	direccion = models.ForeignKey('Direccion', on_delete=models.CASCADE)
-	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
-	avr = models.CharField(max_length=12)
-
-	def __str__(self):
-		return self.nombre
 
 class Materia(models.Model):
 	codigo = models.CharField(max_length=10)	
 	nombre = models.CharField(max_length=60)	
-	avr = models.CharField(max_length=60)
+	# avr = models.CharField(max_length=60)
 	# @TODO: averiguar para que son estos campos
-	u_c = models.IntegerField()	
-	h_s = models.IntegerField()
+	# u_c = models.IntegerField()	
+	# h_s = models.IntegerField()
 	pensum = models.ForeignKey('Pensum', on_delete=models.CASCADE)	
 	# @TODO: averiguar como se usa este campo
-	nivel = models.IntegerField()
-	departamento = models.ForeignKey('Departamento', on_delete=models.CASCADE)
+	# nivel = models.IntegerField()
+	# departamento = models.ForeignKey('Departamento', on_delete=models.CASCADE)
 
 	def __str__(self):
-		return '%s(%s)' % (self.nombre, self.codigo)		
+		return '%s(%s)' % (self.nombre, self.codigo)
+
+class Horas(models.Model):
+	hora = models.IntegerField()
+
+	def __str__(self):
+		return self.horas	
+
+class Minutos(models.Model):
+	minutos = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.mintos	
+
+class BloqueHorario(models.Model):
+	hora_inicio = models.ForeignKey('Horas', related_name='hora_inicio_bloque', on_delete=models.CASCADE)
+	minutos_inicio = models.ForeignKey('Minutos', related_name='minutos_inicio_bloque', on_delete=models.CASCADE)
+	hora_fin = models.ForeignKey('Horas', on_delete=models.CASCADE)
+	minutos_fin = models.ForeignKey('Minutos', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "%s:%s - %s:%s" % (self.hora_inicio, self.minutos_inicio, self.hora_fin, self.minutos_fin)
+
+class Seccion(models.Model):
+	aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
+	# @TODO: verificar el cambio de este campo
+	# numero = models.IntegerField()
+	materia = models.ForeignKey('Materia', on_delete=models.CASCADE)
+	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
+	cupo = models.IntegerField()
+	turno = models.ForeignKey('Turno', on_delete=models.CASCADE)
+	bloque = models.ForeignKey('BloqueHorario', on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "Seccion %s - %s" % (self.numero, self.materia)	
+
+class Docente(models.Model):
+	cedula = models.IntegerField()
+	nombres = models.CharField(max_length=60)
+	apellidos = models.CharField(max_length=60)
+	area = models.ForeignKey('Area', on_delete=models.CASCADE)
+	# telf_movil = models.CharField(max_length=20, blank=True, null=True)
+	# telf_casa = models.CharField(max_length=20, blank=True, null=True)
+	email = models.CharField(max_length=100, blank=True, null=True)
+	estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
+	municipio = models.ForeignKey('Estado', on_delete=models.CASCADE, related_name='estado_docente')
+	direccion = models.TextField(blank=True, null=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return '%s %s CI: %s' % (self.nombres, self.apellidos, self.cedula)
+
+class TelefonoDocente(models.Model):
+	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+	telefono = models.CharField(max_length=15)
+	tipo = models.CharField(max_length=2, choices=(
+		('ce', 'Celular'),
+		('ca', 'Casa'),
+	), default='ce')
+	
+	def __str__(self):
+		return self.telefono
+
+class DocentesSecciones(models.Model):
+	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+	seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE)
+
+
+
+# class Direccion(models.Model):
+# 	nombre = models.CharField(max_length=30)
+# 	area = models.ForeignKey('Area', on_delete=models.CASCADE)
+
+# class Departamento(models.Model):
+# 	nombre = models.CharField(max_length=30)
+# 	direccion = models.ForeignKey('Direccion', on_delete=models.CASCADE)
+# 	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+# 	avr = models.CharField(max_length=12)
+
+# 	def __str__(self):
+# 		return self.nombre
+
+# class Materia(models.Model):
+# 	codigo = models.CharField(max_length=10)	
+# 	nombre = models.CharField(max_length=60)	
+# 	avr = models.CharField(max_length=60)
+# 	# @TODO: averiguar para que son estos campos
+# 	u_c = models.IntegerField()	
+# 	h_s = models.IntegerField()
+# 	pensum = models.ForeignKey('Pensum', on_delete=models.CASCADE)	
+# 	# @TODO: averiguar como se usa este campo
+# 	nivel = models.IntegerField()
+# 	departamento = models.ForeignKey('Departamento', on_delete=models.CASCADE)
+
+# 	def __str__(self):
+# 		return '%s(%s)' % (self.nombre, self.codigo)		
 
 class Turno(models.Model):
 	nombre = models.CharField(max_length=60)
@@ -75,38 +169,38 @@ class Turno(models.Model):
 	def __str__(self):
 		return self.nombre	
 
-class Seccion(models.Model):
-	aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
-	# @TODO: verificar el cambio de este campo
-	numero = models.IntegerField()
-	materia = models.ForeignKey('Materia', on_delete=models.CASCADE)
-	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
-	cupo = models.IntegerField()
-	turno = models.ForeignKey('Turno', on_delete=models.CASCADE)
-	# @TODO: consultar este cambio de haber agregado este campo aqui
-	bloque = models.ForeignKey('Bloque', on_delete=models.CASCADE)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+# class Seccion(models.Model):
+# 	aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
+# 	# @TODO: verificar el cambio de este campo
+# 	numero = models.IntegerField()
+# 	materia = models.ForeignKey('Materia', on_delete=models.CASCADE)
+# 	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
+# 	cupo = models.IntegerField()
+# 	turno = models.ForeignKey('Turno', on_delete=models.CASCADE)
+# 	# @TODO: consultar este cambio de haber agregado este campo aqui
+# 	bloque = models.ForeignKey('Bloque', on_delete=models.CASCADE)
+# 	created = models.DateTimeField(auto_now_add=True)
+# 	updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return "Seccion %s - %s" % (self.numero, self.materia)
+# 	def __str__(self):
+# 		return "Seccion %s - %s" % (self.numero, self.materia)
 
-class TipoAula(models.Model):
-	nombre = models.CharField(max_length=60)
-	descripcion = models.TextField(blank=True, null=True)
-	modalidad = models.IntegerField()
+# class TipoAula(models.Model):
+# 	nombre = models.CharField(max_length=60)
+# 	descripcion = models.TextField(blank=True, null=True)
+# 	modalidad = models.IntegerField()
 
-	def __str__(self):
-		return self.nombre	
+# 	def __str__(self):
+# 		return self.nombre	
 
-class Encuentro(models.Model):
-	# materia = models.ForeignKey('Materia', on_delete=models.CASCADE)
-	cant_horas = models.IntegerField()
-	tipo_aula = models.ForeignKey('TipoAula', on_delete=models.CASCADE)
+# class Encuentro(models.Model):
+# 	# materia = models.ForeignKey('Materia', on_delete=models.CASCADE)
+# 	cant_horas = models.IntegerField()
+# 	tipo_aula = models.ForeignKey('TipoAula', on_delete=models.CASCADE)
 
-class EncuentrosSeccion(models.Model):
-	seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE)
-	encuentro = models.ForeignKey('Encuentro', on_delete=models.CASCADE)
+# class EncuentrosSeccion(models.Model):
+# 	seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE)
+# 	encuentro = models.ForeignKey('Encuentro', on_delete=models.CASCADE)
 
 class Estado(models.Model):
 	nombre = models.CharField(max_length=100)
@@ -120,33 +214,33 @@ class Municipio(models.Model):
 	def __str__(self):
 		return self.nombre	
 
-class Docente(models.Model):
-	cedula = models.IntegerField()
-	nombres = models.CharField(max_length=60)
-	apellidos = models.CharField(max_length=60)
-	area = models.ForeignKey('Area', on_delete=models.CASCADE)
-	telf_movil = models.CharField(max_length=20, blank=True, null=True)
-	telf_casa = models.CharField(max_length=20, blank=True, null=True)
-	email = models.CharField(max_length=100, blank=True, null=True)
-	estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
-	municipio = models.ForeignKey('Estado', on_delete=models.CASCADE, related_name='estado_docente')
-	direccion = models.TextField(blank=True, null=True)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+# class Docente(models.Model):
+# 	cedula = models.IntegerField()
+# 	nombres = models.CharField(max_length=60)
+# 	apellidos = models.CharField(max_length=60)
+# 	area = models.ForeignKey('Area', on_delete=models.CASCADE)
+# 	telf_movil = models.CharField(max_length=20, blank=True, null=True)
+# 	telf_casa = models.CharField(max_length=20, blank=True, null=True)
+# 	email = models.CharField(max_length=100, blank=True, null=True)
+# 	estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
+# 	municipio = models.ForeignKey('Estado', on_delete=models.CASCADE, related_name='estado_docente')
+# 	direccion = models.TextField(blank=True, null=True)
+# 	created = models.DateTimeField(auto_now_add=True)
+# 	updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return '%s %s CI: %s' % (self.nombres, self.apellidos, self.cedula)
+# 	def __str__(self):
+# 		return '%s %s CI: %s' % (self.nombres, self.apellidos, self.cedula)
 
-class DocentesSecciones(models.Model):
-	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
-	seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE)
+# class DocentesSecciones(models.Model):
+# 	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+# 	seccion = models.ForeignKey('Seccion', on_delete=models.CASCADE)
 
-class Asistencia(models.Model):
-	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
-	encuentros_seccion = models.ForeignKey('EncuentrosSeccion', on_delete=models.CASCADE)
-	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
-	asistio = models.BooleanField(default=False)
-	fecha = models.DateField()
+# class Asistencia(models.Model):
+# 	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
+# 	encuentros_seccion = models.ForeignKey('EncuentrosSeccion', on_delete=models.CASCADE)
+# 	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+# 	asistio = models.BooleanField(default=False)
+# 	fecha = models.DateField()
 
 class UbicacionAula(models.Model):
 	nombre = models.CharField(max_length=60)
@@ -155,74 +249,83 @@ class UbicacionAula(models.Model):
 	def __str__(self):
 		return self.nombre	
 
-# @TODO: entender el uso de esta tabla
-class EsquemaDia(models.Model):
-	nombre = models.CharField(max_length=60)
+# # @TODO: entender el uso de esta tabla
+# class EsquemaDia(models.Model):
+# 	nombre = models.CharField(max_length=60)
 
-# @TODO: entender el uso de esta tabla
-class EsquemaHora(models.Model):
-	nombre = models.CharField(max_length=60)
-	sep_num = models.IntegerField(null=True)
+# # @TODO: entender el uso de esta tabla
+# class EsquemaHora(models.Model):
+# 	nombre = models.CharField(max_length=60)
+# 	sep_num = models.IntegerField(null=True)
 
-class Aula(models.Model):
-	nombre = models.CharField(max_length=60)
-	tipo_aula = models.ForeignKey('TipoAula', on_delete=models.CASCADE)
-	ubicacion = models.ForeignKey('UbicacionAula', on_delete=models.CASCADE)
-	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
-	esquema_dia = models.ForeignKey('EsquemaDia', on_delete=models.CASCADE)
-	esquema_hora = models.ForeignKey('EsquemaHora', on_delete=models.CASCADE)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+# class Aula(models.Model):
+# 	nombre = models.CharField(max_length=60)
+# 	tipo_aula = models.ForeignKey('TipoAula', on_delete=models.CASCADE)
+# 	ubicacion = models.ForeignKey('UbicacionAula', on_delete=models.CASCADE)
+# 	proyecto = models.ForeignKey('Proyecto', on_delete=models.CASCADE)
+# 	esquema_dia = models.ForeignKey('EsquemaDia', on_delete=models.CASCADE)
+# 	esquema_hora = models.ForeignKey('EsquemaHora', on_delete=models.CASCADE)
+# 	created = models.DateTimeField(auto_now_add=True)
+# 	updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return '%s - %s - %s' % (self.nombre, self.tipo_aula, self.ubicacion)
+# 	def __str__(self):
+# 		return '%s - %s - %s' % (self.nombre, self.tipo_aula, self.ubicacion)
 
-# @TODO: entender el uso de esta tabla
-class Dia(models.Model):
-	numero = models.IntegerField(null=True)
-	nombre = models.CharField(max_length=12)
-	esquema_dia = models.ForeignKey('EsquemaDia', on_delete=models.CASCADE)
+# # @TODO: entender el uso de esta tabla
+# class Dia(models.Model):
+# 	numero = models.IntegerField(null=True)
+# 	nombre = models.CharField(max_length=12)
+# 	esquema_dia = models.ForeignKey('EsquemaDia', on_delete=models.CASCADE)
 
-class Hora(models.Model):
-	# @TODO: entender el uso de este campo
-	numero = models.IntegerField(null=True)
-	inicio = models.TimeField(null=True)
-	fin = models.TimeField(null=True)
-	# @TODO: entender el uso de este campo
-	esquema_hora = models.ForeignKey('EsquemaHora', on_delete=models.CASCADE)
+# class Hora(models.Model):
+# 	# @TODO: entender el uso de este campo
+# 	numero = models.IntegerField(null=True)
+# 	inicio = models.TimeField(null=True)
+# 	fin = models.TimeField(null=True)
+# 	# @TODO: entender el uso de este campo
+# 	esquema_hora = models.ForeignKey('EsquemaHora', on_delete=models.CASCADE)
 
-	def __str__(self):
-		return '%s - %s' % (self.inicio, self.fin)
+# 	def __str__(self):
+# 		return '%s - %s' % (self.inicio, self.fin)
 
-class HorasTurnos(models.Model):
-	hora = models.ForeignKey('Hora', on_delete=models.CASCADE)
-	turno = models.ForeignKey('Turno', on_delete=models.CASCADE)
+# class HorasTurnos(models.Model):
+# 	hora = models.ForeignKey('Hora', on_delete=models.CASCADE)
+# 	turno = models.ForeignKey('Turno', on_delete=models.CASCADE)
 
 
-# @TODO: esta tabla y la de hora quizas sean redundantes
-class Bloque(models.Model):
-	# aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
-	# @TODO: entender el uso de este campo
-	dia = models.ForeignKey('Dia', on_delete=models.CASCADE)
-	hora = models.ForeignKey('Hora', on_delete=models.CASCADE)
-	activo = models.BooleanField(default=True)
-	# encuentros_seccion = models.ForeignKey('EncuentrosSeccion', on_delete=models.CASCADE)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+# # @TODO: esta tabla y la de hora quizas sean redundantes
+# class Bloque(models.Model):
+# 	# aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
+# 	# @TODO: entender el uso de este campo
+# 	dia = models.ForeignKey('Dia', on_delete=models.CASCADE)
+# 	hora = models.ForeignKey('Hora', on_delete=models.CASCADE)
+# 	activo = models.BooleanField(default=True)
+# 	# encuentros_seccion = models.ForeignKey('EncuentrosSeccion', on_delete=models.CASCADE)
+# 	created = models.DateTimeField(auto_now_add=True)
+# 	updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return '%s - %s' % (self.hora.inicio, self.hora.fin)
+# 	def __str__(self):
+# 		return '%s - %s' % (self.hora.inicio, self.hora.fin)
 
-class Ficha(models.Model):
-	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
-	ingreso = models.TimeField()
-	contrato = models.CharField(max_length=50)
-	categoria = models.CharField(max_length=50)
-	dedicacion = models.CharField(max_length=50)
-	observacion = models.TextField()
-	estatus = models.BooleanField(default=True)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+# class Ficha(models.Model):
+# 	docente = models.ForeignKey('Docente', on_delete=models.CASCADE)
+# 	ingreso = models.TimeField()
+# 	contrato = models.CharField(max_length=50)
+# 	categoria = models.CharField(max_length=50)
+# 	dedicacion = models.CharField(max_length=50)
+# 	observacion = models.TextField()
+# 	estatus = models.BooleanField(default=True)
+# 	created = models.DateTimeField(auto_now_add=True)
+# 	updated = models.DateTimeField(auto_now=True)
+
+
+
+
+
+# --------------
+
+
+
 
 # @TODO: entener el uso de esta tabla
 
