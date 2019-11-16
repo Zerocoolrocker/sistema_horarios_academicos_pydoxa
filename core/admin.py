305dedb728b36753b05858bc5e26dbde445fa833
Ricardo Bello <zerocoolrocker@gmail.com>
2019-11-16 08:24:52 -0430

    Se implementa historial de acciones en admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
 import core.models
 
 for modelo in dir(core.models):
-	if getattr(core.models, modelo).__class__.__name__ == 'ModelBase' and getattr(core.models, modelo).__name__ != 'User':
-		try:
-			admin.site.register(getattr(core.models, modelo))
-		except:
-			pass
+    if getattr(core.models, modelo).__class__.__name__ == 'ModelBase' and getattr(core.models, modelo).__name__ != 'User':
+        try:
+            admin.site.register(getattr(core.models, modelo))
+        except:
+            pass
+
+class LogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ('content_type',
+        'user',
+        'action_time',
+        'object_id',
+        'object_repr',
+        'action_flag',
+        'change_message'
+    )
+    list_display = ('action_flag', 'action_time', 'user', 'change_message', 'object_repr', 'object_id')
+
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    # def get_actions(self, request):
+    #     actions = super(LogEntryAdmin, self).get_actions(request)
+    #     del actions['delete_selected']
+    #     return actions
+
+admin.site.register(LogEntry, LogEntryAdmin)
