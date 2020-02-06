0f842c124fe4e958085ed970f551e288ceb1be2f
Ricardo Bello <zerocoolrocker@gmail.com>
2020-02-06 17:56:41 +0000

    cierre de menu desplegable

--- core/static/menu_desplegable/js/index.js
+++ core/static/menu_desplegable/js/index.js
@@ -24,6 +24,17 @@ dropdownArray.forEach(function(el){
 	};
 })
 
+
+$(window).click(function(){
+	var menu = $('.dropdown .show')[0];
+	var arrow = $('.dropdown .icon-arrow')[0];
+	menu.classList.remove('show');
+	menu.classList.add('hide');
+	arrow.classList.remove('open');
+	arrow.classList.add('close');
+});
+
+
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
